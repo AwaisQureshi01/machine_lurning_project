4e869bc45eff7fada7ef42f9982ec2523e58d3af
AwaisQureshi01 <awaisq576@gmail.com>
2023-08-21 02:40:41 -0700

    data ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,6 @@
 import logging
 import os
 from datetime import datetime
-import sys
-
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
@@ -18,3 +16,5 @@ logging.basicConfig(
 
 )
 
+if __name__ == "__main__":
+    logging.info("logging has started")
